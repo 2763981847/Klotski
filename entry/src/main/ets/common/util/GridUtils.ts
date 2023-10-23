541c120c5dbde095b88474f160f934a26cacb683
Oreki <2763981847@qq.com>
2023-10-23 11:36:24 +0800

    添加了一些注释

--- entry/src/main/ets/common/util/GridUtils.ts
+++ entry/src/main/ets/common/util/GridUtils.ts
@@ -1,9 +1,18 @@
+/**
+ * 网格组件工具类
+ */
 export default class GridUtils {
+
+  /**
+   * 生成一个等距网格的模板字符串，用于网格布局
+   * @param total 给定的行数或列数
+   * @returns 模板字符串
+   */
   public static generateIsometricTemplate(total: number): string {
-    let result: string = '1fr'
-    for (let i = 1;i < total; i++) {
-      result += ' 1fr'
+    let result: string = '1fr';
+    for (let i = 1; i < total; i++) {
+      result += ' 1fr';
     }
-    return result
+    return result;
   }
 }
